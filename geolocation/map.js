ad5bb37e4e93ae1fa534309cc9099a7010bd964c
DRC <drc@email.com>
2015-06-25 09:38:42 -0300

    map

---
+++ geolocation/map.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+	navigator.geolocation.getCurrentPosition(function(geoPosition){
+
+		addMap(geoPosition.coords.latitude, geoPosition.coords.longitude);
+
+	});
+
+	function addMap(latitude, longitude){
+		var latLng = new google.maps.LatLng(latitude,longitude);
+
+		var map = new google.maps.Map(document.querySelector('.container'), {
+			zoom: 16,
+			center: latLng
+		});
+
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: map,
+			title: 'Here !!!'
+		});
+	}
+
+
+};
